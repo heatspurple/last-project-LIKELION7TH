be880433c192184f354ddc61e79928af8f3b698c
Heatspurple <soonsoo564@gmail.com>
2019-07-31 12:15:02 +0900

    heroku

--- ideaproject/settings.py
+++ ideaproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'z(a=ceim2n_=xc+#ct&v1$2a1%!=us
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com',]
 
 
 # Application definition
